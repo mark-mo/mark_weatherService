3a19f5a8817e73f889ede8b0e9315c63f3ee1360
MSI\Mark <marksmott@gmail.com>
2018-03-08 12:08:03 -0700

    Readings inputted

--- src/com/mark/business/WeatherBusiness.java
+++ src/com/mark/business/WeatherBusiness.java
@@ -1,5 +1,7 @@
 package com.mark.business;
 
+import java.util.List;
+
 import com.mark.beans.WeatherSensorModel;
 import com.mark.data.WeatherDAO;
 import com.mark.exception.BadLoginException;
@@ -9,5 +11,10 @@ public class WeatherBusiness {
 	public boolean save(WeatherSensorModel model) {
 		WeatherDAO dao = new WeatherDAO();
 		return dao.create(model);
+	}
+
+	public List<WeatherSensorModel> getReadings() {
+		WeatherDAO dao = new WeatherDAO();
+		return dao.findAll();
 	}
 }
--- src/com/mark/rest/RestService.java
+++ src/com/mark/rest/RestService.java
@@ -1,5 +1,7 @@
 package com.mark.rest;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,6 +13,7 @@ import com.mark.beans.ResponseDataModel;
 import com.mark.beans.ResponseModel;
 import com.mark.beans.WeatherSensorModel;
 import com.mark.business.WeatherBusiness;
+import com.mark.exception.DatabaseErrorException;
 import com.mark.util.HtmlCode;
 
 /**
@@ -70,127 +73,27 @@ public class RestService {
 			// Return OK Response
 			ResponseModel response = new ResponseModel(HtmlCode.Success.getIdentifier(), "OK");
 			return response;
-		}
-		else {
+		} else {
 			ResponseModel response = new ResponseModel(HtmlCode.BadRequest.getIdentifier(), "Bad Request");
 			return response;
 		}
 	}
 
-	/**
-	 * Get Sensor Data in a Date Range at /get/{device}/YYYY-MM-dd HH:mm:ss using
-	 * HTTP GET.
-	 * 
-	 * @param deviceID
-	 *            Device ID to query Sensor Data from
-	 * @param from
-	 *            Start date and time to query where format is YYYY-MM-dd HH:mm:ss
-	 * @param to
-	 *            End date and time to query where format is YYYY-MM-dd HH:mm:ss
-	 * @return Response Model with error code of 0 for no error, -1 if no sensor
-	 *         data found, -2 if database error, -3 if input data parsing error
-	 */
-	// @GET
-	// @Path("/get/{device}/{from}/{to}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public ResponseDataModel getTemperatureSensorDataInRange(@PathParam("device")
-	// int deviceID, @PathParam("from") String from, @PathParam("to") String to)
-	// {
-	// // Log the API call
-	// logger.info("Entering RestService.getTemperatureSensorDataInRange()");
-	// logger.debug("Device ID: " + deviceID + " From: " + from + " To: " + to);
-	//
-	// // Parse parameters and call Business Service to get the Sensor Data in a
-	// Date Range
-	// SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	// format.setTimeZone(TimeZone.getTimeZone("US/Arizona"));
-	// try
-	// {
-	// // Call Business Service to get the Sensor Data in a Date Range
-	// Date fromDate = format.parse(from);
-	// Date toDate = format.parse(to);
-	// List<WeatherSensorModel>data =
-	// service.getTemperatureSensorDataInRange(deviceID, fromDate, toDate);
-	//
-	// // Return Response and Data
-	// int returnError = 0;
-	// String returnMessage = "OK";
-	// ResponseDataModel response = new ResponseDataModel(returnError,
-	// returnMessage, data);
-	// return response;
-	// }
-	// catch (NotFoundException e)
-	// {
-	// // Return Database Error Response
-	// ResponseDataModel response = new ResponseDataModel(-1, "Sensor Data Not
-	// Found" + e.getMessage(), new ArrayList<WeatherSensorModel>());
-	// return response;
-	// }
-	// catch (DAOException e)
-	// {
-	// // Return Database Error Response
-	// ResponseDataModel response = new ResponseDataModel(-2, "Database Exception: "
-	// + e.getMessage(), new ArrayList<WeatherSensorModel>());
-	// return response;
-	// }
-	// catch (ParseException e)
-	// {
-	// // Return Input Data Parameter Error Response
-	// ResponseDataModel response = new ResponseDataModel(-3, "Bad Parameter Value",
-	// new ArrayList<WeatherSensorModel>());
-	// return response;
-	// }
-	// }
+	@GET
+	@Path("/readings")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ResponseModel getReadings() {
+		WeatherBusiness business = new WeatherBusiness();
+		ResponseModel response;
 
-	/**
-	 * Get Sensor Data for a specified Sensor Device ID at /get/{device}/{id} using
-	 * HTTP GET.
-	 * 
-	 * @param deviceID
-	 *            Device ID to query Sensor Data from
-	 * @param id
-	 *            Sensor ID to return
-	 * @return Response Model with error code of 0 for no error, -1 if no sensor
-	 *         data found, -2 if database error
-	 */
-	// @GET
-	// @Path("/get/{device}/{id}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public ResponseDataModel getTemperatureSensorData(@PathParam("device") int
-	// deviceID, @PathParam("id") int id)
-	// {
-	// try
-	// {
-	// // Log the API call
-	// logger.info("Entering RestService.getTemperatureSensorData()");
-	// logger.debug("Device ID: " + deviceID + " Sensor Data ID: " + id);
-	//
-	// // Call Business Service to get the Sensor Data for a specified Sensor Data
-	// ID
-	// WeatherSensorModel data = service.getTemperatureSensorData(deviceID, id);
-	//
-	// // Return Response and Data
-	// int returnError = 0;
-	// String returnMessage = "OK";
-	// List<WeatherSensorModel> returnData = new ArrayList<WeatherSensorModel>();
-	// returnData.add(data);
-	// ResponseDataModel response = new ResponseDataModel(returnError,
-	// returnMessage, returnData);
-	// return response;
-	// }
-	// catch (NotFoundException e)
-	// {
-	// // Return Database Error Response
-	// ResponseDataModel response = new ResponseDataModel(-1, "Sensor Data Not
-	// Found" + e.getMessage(), new ArrayList<WeatherSensorModel>());
-	// return response;
-	// }
-	// catch (DAOException e)
-	// {
-	// // Return Database Error Response
-	// ResponseDataModel response = new ResponseDataModel(-2, "Database Exception: "
-	// + e.getMessage(), new ArrayList<WeatherSensorModel>());
-	// return response;
-	// }
-	// }
+		try {
+			List<WeatherSensorModel> readings = business.getReadings();
+			response = new ResponseDataModel<List<WeatherSensorModel>>(HtmlCode.Success.getIdentifier(), "OK",
+					readings);
+		} catch (DatabaseErrorException e) {
+			response = new ResponseModel(HtmlCode.BadRequest.getIdentifier(), "Error retrieving readings");
+		}
+
+		return response;
+	}
 }
